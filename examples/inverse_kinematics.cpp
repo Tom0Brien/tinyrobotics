5398b5eb4678395507e0956939e8c5f4dace1d00
tom <thomas16441@gmail.com>
2023-03-15 12:19:33 +1100

    Clean up IK example

--- examples/inverse_kinematics.cpp
+++ examples/inverse_kinematics.cpp
@@ -1,11 +1,4 @@
-#include <autodiff/forward/real.hpp>
-#include <autodiff/forward/real/eigen.hpp>
-#include <cassert>
 #include <chrono>
-#include <cstdlib>
-#include <iostream>
-#include <nlohmann/json.hpp>
-#include <string>
 
 #include "../include/Dynamics.hpp"
 #include "../include/InverseKinematics.hpp"
@@ -15,16 +8,10 @@
 #include "../include/Parser.hpp"
 #include "../include/Solver.hpp"
 
-
-std::string swing_foot   = "right_foot";
-std::string planted_foot = "left_foot";
-std::string world_frame  = "world";
-
 int main(int argc, char* argv[]) {
     // Load in Model from URDF
-    std::string path_to_urdf = "../data/urdfs/robot.urdf";
-    const int n_joints       = 20;
-    auto model               = tr::model_from_urdf<double, n_joints>(path_to_urdf);
+    const int n_joints = 20;
+    auto model         = tr::model_from_urdf<double, n_joints>("../data/urdfs/robot.urdf");
 
     // Set initial conditions for the robot
     auto q0 = model.home_configuration();
@@ -35,41 +22,43 @@ int main(int argc, char* argv[]) {
     auto Htl_0        = tr::forward_kinematics(model, q0, target_frame);
 
     // Compute the inverse kinematics for moving the left foot upwards
-    Eigen::Matrix<double, 3, 1> target_position;
     auto Htl_target = Htl_0;
     auto q          = q0;
-    std::vector<Eigen::Matrix<double, 20, 1>> q_history;
+    std::vector<Eigen::Matrix<double, n_joints, 1>> q_history;
 
-    // Start timer
-    auto start                     = std::chrono::high_resolution_clock::now();
+
     double total_translation_error = 0;
     double total_rotation_error    = 0;
-    for (int i = 0; i < 10; i++) {
-        // Add 0.01m to the x and z position
-        Htl_target(0, 3) += 0.01;
-        Htl_target(2, 3) += 0.01;
+    double total_time              = 0;
+    const int n_iterations         = 10;
+    for (int i = 0; i < n_iterations; i++) {
+        // Move the target position
+        Htl_target(0, 3) += 0.05;
+        Htl_target(2, 3) += 0.05;
 
         // Compute the inverse kinematics
-        q = tr::inverse_kinematics(model, source_frame, target_frame, Htl_target, q);
-        q_history.push_back(q);
+        // Start timer
+        auto start = std::chrono::high_resolution_clock::now();
+        q          = tr::inverse_kinematics(model, source_frame, target_frame, Htl_target, q);
+        // Stop timer
+        auto stop     = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+        total_time += duration.count();
 
         // Print out the current configuration
         auto Htl = tr::forward_kinematics(model, q, target_frame);
 
         // Compute the error
-        Eigen::Quaterniond quaternion_current(Htl.rotation());
-        Eigen::Quaterniond quaternion_target(Htl_target.rotation());
         auto translation_error = (Htl_target.translation() - Htl.translation()).norm();
-        auto rotation_error    = tr::quaternion_error(quaternion_current, quaternion_target);
+        auto rotation_error =
+            (Eigen::Matrix<double, 3, 3>::Identity() - Htl_target.rotation() * Htl.rotation().transpose())
+                .diagonal()
+                .sum();
         total_translation_error += translation_error;
         total_rotation_error += rotation_error;
-        std::cout << "(Translation error, Rotation error): (" << translation_error << ", " << rotation_error << ")"
-                  << " at iteration " << i << std::endl;
     }
-    // Stop timer
-    auto stop     = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << "Total time taken by IK for 10 iterations: " << duration.count() << " milliseconds" << std::endl;
+
+    std::cout << "Average time taken by IK: " << total_time / n_iterations << " milliseconds" << std::endl;
     std::cout << "Average translation error: " << total_translation_error / 10 << std::endl;
     std::cout << "Average rotation error: " << total_rotation_error / 10 << std::endl;
 
